edcacfadec1e06011581ce71f49076cec0ceef9a
LukazDs <lukazds2016@gmail.com>
2022-09-14 23:14:17 -0300

    feat: create signup route request body validation

---
+++ src/routers/userRouter.ts
@@ -0,0 +1,10 @@
+import joi from "joi";
+import { IUserRequestBody } from "../utils/sqlUserUtils";
+
+const userSignUpSchema = joi.object<IUserRequestBody>({
+    email: joi.string().email().required(),
+    password: joi.string().min(8).required(),
+    confirmedPassword: joi.ref("password")
+})
+
+export default userSignUpSchema ;
