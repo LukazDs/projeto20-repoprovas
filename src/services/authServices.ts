2694729f0841722c0de933e113605f2548d2a5f0
LukazDs <lukazds2016@gmail.com>
2022-09-15 01:24:22 -0300

    generate token on login

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,7 +1,7 @@
 import "express-async-errors";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
-//import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { IUser, IUserRequestBody } from "../utils/sqlUserUtils";
 import * as authRepository from "../repositories/authRepository";
 import { User } from "@prisma/client";
@@ -12,12 +12,12 @@ async function comparePassword(password: string, passwordDb: string) {
 
     const passwordValidation: boolean = bcrypt.compareSync(password, passwordDb);
 
-    if(!passwordValidation) {
+    if (!passwordValidation) {
 
-        throw {code: "Unauthorized", message: "Email ou password incorretos!"};
-    
+        throw { code: "Unauthorized", message: "Email ou password incorretos!" };
+
     }
-    
+
 }
 
 export async function findUser(user: IUser) {
@@ -26,11 +26,20 @@ export async function findUser(user: IUser) {
 
     await comparePassword(user.password, users[0].password);
 
-    return "token gerado"
+    const token = await getToken(user);
 
-   
+    return token;
+
 }
 
 
+export async function getToken(user: IUser) {
 
+    const JWT_PASSWORD: string = String(process.env.JWT_KEY);
+    const TIME: string = String(process.env.JWT_TIME)
 
+    const token: string = jwt.sign(user, JWT_PASSWORD, { expiresIn: TIME });
+
+    return { token };
+
+}
