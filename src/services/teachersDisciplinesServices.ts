09bc84452d780fe046b3e17b66b4a52fb1affbbe
LukazDs <lukazds2016@gmail.com>
2022-09-16 23:54:06 -0300

    feat: get teachers discipline

--- src/services/teachersDisciplinesServices.ts
+++ src/services/teachersDisciplinesServices.ts
@@ -1,15 +1,33 @@
 import * as teachersDisciplinesRepository from "../repositories/teachersDisciplinesRepository";
 import { TeachersDiscipline } from "@prisma/client";
 
-export async function findTeachersDiscipline(disciplineId: number, teacherId: number) {
+export async function findTeachersDiscipline(
+  disciplineId: number,
+  teacherId: number
+) {
+  const teachersDisciplines: TeachersDiscipline[] =
+    await teachersDisciplinesRepository.findTeachersDisciplines(
+      disciplineId,
+      teacherId
+    );
 
-    const teachersDisciplines: TeachersDiscipline[] = await teachersDisciplinesRepository
-        .findTeachersDisciplinesByName(disciplineId, teacherId);
+  if (!teachersDisciplines.length) {
+    throw {
+      code: "NotFound",
+      message: "Professor ou disciplina não vinculados!",
+    };
+  }
 
-    if (!teachersDisciplines.length) {
-        throw { code: "NotFound", message: "Professor ou disciplina não vinculados!" };
-    }
-
-    return teachersDisciplines[0];
+  return teachersDisciplines[0];
 }
 
+export async function findTeachersDisciplinesByDisciplineId(
+  disciplineId: number
+) {
+  const teachersDisciplines: TeachersDiscipline[] =
+    await teachersDisciplinesRepository.findTeachersDisciplinesByDisciplineId(
+      disciplineId
+    );
+
+  return teachersDisciplines[0];
+}
