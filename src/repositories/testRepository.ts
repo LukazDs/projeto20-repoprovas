d397afdc62c1fecf197b23bd000f1362df353147
LukazDs <lukazds2016@gmail.com>
2022-09-16 23:59:53 -0300

    feat: get tests by teachersDisciplineId

--- src/repositories/testRepository.ts
+++ src/repositories/testRepository.ts
@@ -2,7 +2,11 @@ import { prisma } from "../config/database";
 import { ITest } from "../utils/sqlTestUtils";
 
 export async function insertTest(test: ITest) {
+  await prisma.tests.create({ data: test });
+}
 
-    await prisma.tests.create({ data: test });
-
+export async function findTestsByTeachersDisciplineId(
+  teachersDisciplineId: number
+) {
+  await prisma.tests.findMany({ where: { teachersDisciplineId } });
 }
