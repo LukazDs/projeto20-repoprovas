16a6bd3380333f2363a57a5fa5e97730e66567da
LukazDs <lukazds2016@gmail.com>
2022-09-17 20:36:33 -0300

    feat: add teacher informations

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import * as testServices from "../services/testServices";
+import * as teacherServices from "../services/teacherServices";
 import * as disciplineServices from "../services/disciplineServices";
 import * as teachersDiscipline from "../services/teachersDisciplinesServices";
 import { ITestReqBody } from "../utils/sqlTestUtils";
-import { TeachersDiscipline, Tests } from "@prisma/client";
+import { Teacher, TeachersDiscipline, Tests } from "@prisma/client";
 
 export async function insertTest(req: Request, res: Response) {
   const test: ITestReqBody = req.body;
@@ -13,7 +14,7 @@ export async function insertTest(req: Request, res: Response) {
   res.status(201).send("Teste criado!");
 }
 
-export async function getTestsbyDisciplineId(req: Request, res: Response) {
+export async function getTestsByDisciplineId(req: Request, res: Response) {
   const disciplineId: number = Number(req.params.disciplineId);
 
   await disciplineServices.findDisciplineById(disciplineId);
@@ -23,9 +24,13 @@ export async function getTestsbyDisciplineId(req: Request, res: Response) {
       disciplineId
     );
 
+  const teachers: Teacher = await teacherServices.findTeacherById(
+    teachersDisciplines.teacherId
+  );
+
   const tests: Tests[] = await testServices.findTestsByTeachersDisciplineId(
     teachersDisciplines.id
   );
 
-  res.status(200).send(tests);
+  res.status(200).send({ tests, teachers });
 }
--- src/services/teacherServices.ts
+++ src/services/teacherServices.ts
@@ -4,9 +4,21 @@ import { Teacher } from "@prisma/client";
 export async function findTeacherByName(name: string) {
   const teachers: Teacher[] = await teacherRepository.findTeacherByName(name);
 
-  if (!teachers.length) {
-    throw { code: "NotFound", message: "Professor não encontrado!" };
-  }
+  await checkArrayLength(teachers);
 
   return teachers[0];
+}
+
+export async function findTeacherById(id: number) {
+  const teachers: Teacher[] = await teacherRepository.findTeacherById(id);
+
+  await checkArrayLength(teachers);
+
+  return teachers[0];
+}
+
+async function checkArrayLength(arr: Teacher[]) {
+  if (!arr.length) {
+    throw { code: "NotFound", message: "Professor não encontrado!" };
+  }
 }
