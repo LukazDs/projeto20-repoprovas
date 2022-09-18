30819d1e483fdd25807fe6771a4a8dfccd011224
LukazDs <lukazds2016@gmail.com>
2022-09-17 23:00:51 -0300

    feat: get teacher by id

--- src/controllers/teacherController.ts
+++ src/controllers/teacherController.ts
@@ -7,3 +7,10 @@ export async function getTeachers(_req: Request, res: Response) {
 
   res.status(200).send(teachers);
 }
+
+export async function getTeacherById(req: Request, res: Response) {
+  const id: number = Number(req.params.id);
+  const teacher: Teacher = await teacherServices.findTeacherById(id);
+
+  res.status(200).send(teacher);
+}
