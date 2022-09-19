506d339a5f4a3a40de06942d1eaa74a76882b411
LukazDs <lukazds2016@gmail.com>
2022-09-19 16:52:14 -0300

    refactor: change tests

--- tests/tests_super_test/authSignUp.test.ts
+++ tests/tests_super_test/authSignUp.test.ts
@@ -31,33 +31,6 @@ describe("Testa POST /signup ", () => {
   });
 });
 
-// describe("Testa GET /items ", () => {
-//   it("Deve retornar status 200 e o body no formato de Array", async () => {
-//     await supertest(app).post("/items").send(item1);
-//     const result = await supertest(app).get("/items");
-//     expect(result.status).toBe(200);
-//   });
-// });
-
-// describe("Testa GET /items/:id", () => {
-//   it("Deve retornar status 200 e um objeto igual a o item cadastrado", async () => {
-//     await supertest(app).post("/items").send(item1);
-
-//     const items = await prisma.items.findMany({ where: item1 });
-//     const result = await supertest(app).get(`/items/${items[0].id}`);
-
-//     expect(result.status).toBe(200);
-//   });
-//   it("Deve retornar status 404 caso não exista um item com esse id", async () => {
-//     await supertest(app).post("/items").send(item1);
-
-//     const items = await prisma.items.findMany({ where: item1 });
-//     const result = await supertest(app).get(`/items/${items[0].id + 1}`);
-
-//     expect(result.status).toBe(404);
-//   });
-// });
-
 afterAll(async () => {
   await prisma.$disconnect();
 });
